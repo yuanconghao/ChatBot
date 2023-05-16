d18448ee63675608d47dc7216bb78304613d2dfd
yuanconghao <yuanconghao@gmail.com>
2023-05-16 20:28:17 +0800

    add prompt

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -15,52 +15,52 @@ class Chatbot:
         self.temperature = temperature
         self.vectors = vectors
 
-    # qa_template = """
-    #     You are a helpful AI assistant named ChatBot. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
-    #     If you don't know the answer, just say you don't know. Do not try to make up an answer.
-    #     If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-    #     Use as much detail as possible when responding.
-    #
-    #     context: {context}
-    #     =========
-    #     question: {question}
-    #     ======
-    #     """
     qa_template = """
-            You are an intelligent report generator named ChatBot.
-            The user gives you a file its content is represented by the following pieces of context,and I need your help to read and summarize the following questions:
-                1. Please summarize how many vocabulary words the child has speak and its mastery level. 
-                2. Please summarize the interaction and expression between the child and the foreign teacher from the listening, speaking, and reading dimensions.
-                3. Please summarize the areas where the child needs improvement and the corresponding improvement plan.
-                4. Finally, please give me some test questions and suggestions to enhance the effectiveness of today's lesson.
-                5. Please summarize the sentences the child said.
-                6. Based on the child's performance today, please offer some English learning suggestions.
-                7. Based on the child's situation, please suggest an English language game that involves role-playing. 
-                
-                Follow the format of the output that follows:                  
-                 1. Vocabulary: xxx\n\n
-                 2. Interaction: xxx\n\n
-                    - (1) listening: xxx\n\n
-                    - (2) speaking: xxx\n\n
-                    - (3) reading dimensions: xxx\n\n
-                 3. Improvement: xxx\n\n                 
-                 4. Lesson Suggestions: xxx\n\n
-                 5. Summary: \n\n
-                    - (1):xxx;\n 
-                    - (2):xxx;\n 
-                    - (3):xxx;\n  
-                 6. Learning suggestions: \n\n
-                    - (1):xxx;\n 
-                    - (2):xxx;\n 
-                    - (3):xxx;\n
-                 7. English Language Game: xxx\n\n
-                 
-            
-            context: {context}
-            =========
-            question: {question}
-            ======
-            """
+        You are a helpful AI assistant named ChatBot. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
+        If you don't know the answer, just say you don't know. Do not try to make up an answer.
+        If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+        Use as much detail as possible when responding.
+
+        context: {context}
+        =========
+        question: {question}
+        ======
+        """
+    # qa_template = """
+    #         You are an intelligent report generator named ChatBot.
+    #         The user gives you a file its content is represented by the following pieces of context,and I need your help to read and summarize the following questions:
+    #             1. Please summarize how many vocabulary words the child has speak and its mastery level.
+    #             2. Please summarize the interaction and expression between the child and the foreign teacher from the listening, speaking, and reading dimensions.
+    #             3. Please summarize the areas where the child needs improvement and the corresponding improvement plan.
+    #             4. Finally, please give me some test questions and suggestions to enhance the effectiveness of today's lesson.
+    #             5. Please summarize the sentences the child said.
+    #             6. Based on the child's performance today, please offer some English learning suggestions.
+    #             7. Based on the child's situation, please suggest an English language game that involves role-playing.
+    #
+    #             Follow the format of the output that follows:
+    #              1. Vocabulary: xxx\n\n
+    #              2. Interaction: xxx\n\n
+    #                 - (1) listening: xxx\n\n
+    #                 - (2) speaking: xxx\n\n
+    #                 - (3) reading dimensions: xxx\n\n
+    #              3. Improvement: xxx\n\n
+    #              4. Lesson Suggestions: xxx\n\n
+    #              5. Summary: \n\n
+    #                 - (1):xxx;\n
+    #                 - (2):xxx;\n
+    #                 - (3):xxx;\n
+    #              6. Learning suggestions: \n\n
+    #                 - (1):xxx;\n
+    #                 - (2):xxx;\n
+    #                 - (3):xxx;\n
+    #              7. English Language Game: xxx\n\n
+    #
+    #
+    #         context: {context}
+    #         =========
+    #         question: {question}
+    #         ======
+    #         """
 
     QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["context","question" ])
 

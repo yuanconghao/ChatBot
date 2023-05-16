a4c4285d90c81db6b0630946e1627729c8e65a6b
yuanconghao <yuanconghao@gmail.com>
2023-05-16 17:50:50 +0800

    chatbot

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -15,17 +15,33 @@ class Chatbot:
         self.temperature = temperature
         self.vectors = vectors
 
+    # qa_template = """
+    #     You are a helpful AI assistant named ChatBot. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
+    #     If you don't know the answer, just say you don't know. Do not try to make up an answer.
+    #     If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+    #     Use as much detail as possible when responding.
+    #
+    #     context: {context}
+    #     =========
+    #     question: {question}
+    #     ======
+    #     """
     qa_template = """
-        You are a helpful AI assistant named ChatBot. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
-        If you don't know the answer, just say you don't know. Do not try to make up an answer.
-        If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-        Use as much detail as possible when responding.
-
-        context: {context}
-        =========
-        question: {question}
-        ======
-        """
+            You are an intelligent report generator named ChatBot.
+            The user gives you a file its content is represented by the following pieces of context,and you can help me summarize it in the following format:
+                1. Please summarize how many vocabulary words the child has learned and their mastery level.
+                2. Please summarize the interaction and expression between the child and the foreign teacher from the listening, speaking, and reading dimensions.
+                3. Please summarize the areas where the child needs improvement and the corresponding improvement plan.
+                4. Finally, please give me some test questions and suggestions to enhance the effectiveness of today's lesson.
+                5. Please summarize the sentences the child said.
+                6. Based on the child's performance today, please offer some English learning suggestions.
+                7. Based on the child's situation, please suggest an English language game that involves role-playing. 
+            
+            context: {context}
+            =========
+            question: {question}
+            ======
+            """
 
     QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["context","question" ])
 
